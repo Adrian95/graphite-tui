3add0c5bfd5e801010428a5b1e1eb9d5e0562407
Adrian Albrektsen <adrianalbrektsen95@gmail.com>
2026-01-09 20:14:59 +0100

    chore: bump version to v1.7.2

--- version.go
+++ version.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	currentVersion = "v1.7.1"
+	currentVersion = "v1.7.2"
 	repoOwner      = "Adrian95"
 	repoName       = "graphite-tui"
 	githubAPI      = "https://api.github.com/repos/%s/%s/releases/latest"
